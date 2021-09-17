8606de2e8e091ef2835c646945e2b7871ed22bcc
Yusuf Tulek <yusuf@gambl.xyz>
2021-09-18 00:27:19 +0300

    remove obi, fix movement

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -3,28 +3,27 @@ using UnityEngine;
 public class MovementController : MonoBehaviour
 {
 	[SerializeField] Rigidbody _rigidbody;
-	[SerializeField] float _movementSpeedMultiplier;
+	[SerializeField] float _sideMoveSpeedMultiplier;
+	[SerializeField] float _forwardMoveSpeedMultiplier;
 
 	float _lastFrameMousePosX;
 	float _valueToApply;
 	bool _didLastFrameClick;
 	bool _shouldApplyForce;
 	
-
 	void Update()
 	{
 		if (Input.GetMouseButton(0))
 		{
 			if (_didLastFrameClick)
 			{
-				// 1125 pixel, 3meter platform wideness
-				// 375 pixel for 1m
-				
 				// calculate diff between last frame and current
 				float deltaPosX = Input.mousePosition.x - _lastFrameMousePosX;
-				float normalizedDeltaPosX = Mathf.InverseLerp(0f, Screen.width / 3f, Mathf.Abs(deltaPosX));
+				float normalizedDeltaPosX = Mathf.InverseLerp(1f, Screen.width * 0.02f, Mathf.Abs(deltaPosX));
+
 				_valueToApply = deltaPosX > 0 ? normalizedDeltaPosX : -normalizedDeltaPosX;
-				_shouldApplyForce = true;
+				
+				_shouldApplyForce = deltaPosX != 0f;
 				
 				_lastFrameMousePosX = Input.mousePosition.x;
 			}
@@ -46,11 +45,11 @@ public class MovementController : MonoBehaviour
 	{
 		if (_shouldApplyForce)
 		{
-			_rigidbody.AddForce(new Vector3(_valueToApply, 0f, 0f) * _movementSpeedMultiplier * Time.deltaTime, ForceMode.VelocityChange);
+			_rigidbody.AddForce(new Vector3(_valueToApply, 0f, 0f) * _sideMoveSpeedMultiplier * Time.deltaTime, ForceMode.VelocityChange);
 		}
 		else
 		{
-			_rigidbody.velocity = new Vector3(0f, _rigidbody.velocity.y, _rigidbody.velocity.z);
+			_rigidbody.velocity = new Vector3(Mathf.Lerp(_rigidbody.velocity.x, 0f, Time.deltaTime * 5f), _rigidbody.velocity.y, _rigidbody.velocity.z);
 		}
 	}
 }
