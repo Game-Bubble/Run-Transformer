0d0d99305a6b7a9c3eb233b2f6018bda42a710d9
Yusuf Tulek <yusuf@gambl.xyz>
2021-09-18 22:44:49 +0300

    change movement to transform

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -2,20 +2,16 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
-	[SerializeField] Rigidbody _rigidbody;
 	[SerializeField] float _sideMoveSpeedMultiplier;
 	[SerializeField] float _forwardMoveSpeed;
-	[SerializeField] float _maxForwardMoveSpeed;
 
 	float _lastFrameMousePosX;
 	float _valueToApply;
 	bool _didLastFrameClick;
-	bool _shouldApplyForce;
-
+	
 	public void OnTouchPressUp()
 	{
 		_didLastFrameClick = false;
-		_shouldApplyForce = false;
 	}
 
 	public void OnTouchPress()
@@ -24,38 +20,18 @@ public class MovementController : MonoBehaviour
 		{
 			// calculate diff between last frame and current
 			float deltaPosX = Input.mousePosition.x - _lastFrameMousePosX;
-			float normalizedDeltaPosX = Mathf.InverseLerp(1f, Screen.width * 0.01f, Mathf.Abs(deltaPosX));
+			float normalizedDeltaPosX = Mathf.InverseLerp(1f, Screen.width * 0.02f, Mathf.Abs(deltaPosX));
 
 			_valueToApply = deltaPosX > 0 ? normalizedDeltaPosX : -normalizedDeltaPosX;
-
-			_shouldApplyForce = true;
-
+			
 			_lastFrameMousePosX = Input.mousePosition.x;
+			
+			transform.Translate(_valueToApply * _sideMoveSpeedMultiplier * Time.deltaTime, 0f, _forwardMoveSpeed * Time.deltaTime);
 		}
 		else
 		{
 			_lastFrameMousePosX = Input.mousePosition.x;
 			_didLastFrameClick = true;
-		}
-	}
-
-	void FixedUpdate()
-	{
-		if (_shouldApplyForce)
-		{
-			_rigidbody.AddForce(new Vector3(_valueToApply * _sideMoveSpeedMultiplier, 0f, _forwardMoveSpeed) * Time.deltaTime, ForceMode.VelocityChange);
-			if (_rigidbody.velocity.z >= _maxForwardMoveSpeed)
-			{
-				_rigidbody.velocity = new Vector3(_rigidbody.velocity.x, _rigidbody.velocity.y, _maxForwardMoveSpeed);
-			}
-		}
-		else
-		{
-			_rigidbody.velocity = new Vector3(Mathf.Lerp(_rigidbody.velocity.x, 0f, Time.deltaTime * 5f), _rigidbody.velocity.y, Mathf.Lerp(_rigidbody.velocity.z, 0f, Time.deltaTime * 5f));
-			if (_rigidbody.velocity.magnitude < 0.3f)
-			{
-				_rigidbody.velocity = Vector3.zero;
-			}
 		}
 	}
 }
